9941c6b2037844f9faf2b0007033ee09709f2503
Quentin Comte-Gaz <quentin@comte-gaz.com>
2019-11-10 11:56:17 +0100

    Test: Add parse xml file unit-test

--- test/main.cpp
+++ test/main.cpp
@@ -35,19 +35,6 @@ TEST_CASE("Update credentials")
     api.updateCredentials(username, pass);
 }
 
-TEST_CASE("List files (invalid response)")
-{
-    QString username("fakeUser");
-    QString pass("fakePass");
-    QString container = "invalidContainer";
-
-    QAzureStorageRestApi api(username, pass);
-
-    QNetworkReply* reply = api.listFiles(container);
-    REQUIRE(reply != nullptr);
-    REQUIRE(reply->error() == QNetworkReply::NetworkError::NoError);
-}
-
 TEST_CASE("Upload file (invalid path)")
 {
     QString username("fakeUser");
@@ -62,7 +49,20 @@ TEST_CASE("Upload file (invalid path)")
     REQUIRE(reply == nullptr);
 }
 
-TEST_CASE("Upload file (invalid response)")
+TEST_CASE("List files")
+{
+    QString username("fakeUser");
+    QString pass("fakePass");
+    QString container = "invalidContainer";
+
+    QAzureStorageRestApi api(username, pass);
+
+    QNetworkReply* reply = api.listFiles(container);
+    REQUIRE(reply != nullptr);
+    qDebug() << "Error response: " << reply->error();
+}
+
+TEST_CASE("Upload file")
 {
     QString username("fakeUser");
     QString pass("fakePass");
@@ -83,10 +83,10 @@ TEST_CASE("Upload file (invalid response)")
 
     QNetworkReply* reply = api.uploadFile(path, container, blob);
     REQUIRE(reply != nullptr);
-    REQUIRE(reply->error() == QNetworkReply::NetworkError::NoError);
+    qDebug() << "Error response: " << reply->error();
 }
 
-TEST_CASE("Download file (invalid response)")
+TEST_CASE("Download file")
 {
     QString username("fakeUser");
     QString pass("fakePass");
@@ -97,7 +97,77 @@ TEST_CASE("Download file (invalid response)")
 
     QNetworkReply* reply = api.downloadFile(container, blob);
     REQUIRE(reply != nullptr);
-    REQUIRE(reply->error() == QNetworkReply::NetworkError::NoError);
+    qDebug() << "Error response: " << reply->error();
+}
+
+TEST_CASE("Parse file list")
+{
+    QByteArray fileList;
+
+    QList< QMap<QString,QString> > res = QAzureStorageRestApi::parseFileList(fileList);
+    qDebug() << "List: " << res;
+    REQUIRE(res.isEmpty());
+
+    fileList = QByteArray("<?xml version=\"1.0\" encoding=\"utf-8\"?>\
+                            <EnumerationResults ServiceEndpoint=\"http://myaccount.blob.core.windows.net/\"  ContainerName=\"mycontainer\">\
+                              <Prefix>string-value</Prefix>\
+                              <Marker>string-value</Marker>\
+                              <MaxResults>int-value</MaxResults>\
+                              <Delimiter>string-value</Delimiter>\
+                              <Blobs>\
+                                <Blob>\
+                                  <Name>blob-name</Name>\
+                                  <Deleted>true</Deleted>\
+                                  <Snapshot>date-time-value</Snapshot>\
+                                  <Properties>\
+                                    <Creation-Time>date-time-value</Creation-Time>\
+                                    <Last-Modified>date-time-value</Last-Modified>\
+                                    <Etag>etag</Etag>\
+                                    <Content-Length>size-in-bytes</Content-Length>\
+                                    <Content-Type>blob-content-type</Content-Type>\
+                                    <Content-Encoding />\
+                                    <Content-Language />\
+                                    <Content-MD5 />\
+                                    <Cache-Control />\
+                                    <BlobType>BlockBlob|PageBlob|AppendBlob</BlobType>\
+                                    <AccessTier>tier</AccessTier>\
+                                    <LeaseStatus>locked|unlocked</LeaseStatus>\
+                                    <LeaseState>available | leased | expired | breaking | broken</LeaseState>\
+                                    <LeaseDuration>infinite | fixed</LeaseDuration>\
+                                    <CopyId>id</CopyId>\
+                                    <CopyStatus>pending | success | aborted | failed </CopyStatus>\
+                                    <CopySource>source url</CopySource>\
+                                    <CopyProgress>bytes copied/bytes total</CopyProgress>\
+                                    <CopyCompletionTime>datetime</CopyCompletionTime>\
+                                    <CopyStatusDescription>error string</CopyStatusDescription>\
+                                    <ServerEncrypted>true</ServerEncrypted>\
+                                    <IncrementalCopy>true</IncrementalCopy>\
+                                    <AccessTierInferred>true</AccessTierInferred>\
+                                    <AccessTierChangeTime>datetime</AccessTierChangeTime>\
+                                    <DeletedTime>datetime</DeletedTime>\
+                                    <RemainingRetentionDays>no-of-days</RemainingRetentionDays>\
+                                  </Properties>\
+                                  <Metadata>\
+                                    <Name>value</Name>\
+                                  </Metadata>\
+                                </Blob>\
+                                <Blob>\
+                                  <Name>blob-name-2</Name>\
+                                  <FakeKey>FakeValue</FakeKey>\
+                                  <FakeKey2>FakeValue2</FakeKey2>\
+                                </Blob>\
+                                <BlobPrefix>\
+                                  <Name>blob-prefix</Name>\
+                                </BlobPrefix>\
+                              </Blobs>\
+                              <NextMarker />\
+                            </EnumerationResults>");
+
+    res = QAzureStorageRestApi::parseFileList(fileList);
+    qDebug() << "Extracted blobl list: " << res;
+    REQUIRE(!res.isEmpty());
+    REQUIRE(res.count() == 2);
+    REQUIRE(res[1] == QMap<QString,QString>({{"FakeKey", "FakeValue"}, {"FakeKey2", "FakeValue2"}, {"Name", "blob-name-2"}}));
 }
 
 int main(int argc, char* argv[])
