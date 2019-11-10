05b3998dbe935ebee099ae4a14f4f213b94dbd68
Quentin Comte-Gaz <quentin@comte-gaz.com>
2019-11-10 11:16:04 +0100

    Test: Add some unit-test

--- test/main.cpp
+++ test/main.cpp
@@ -23,6 +23,83 @@ TEST_CASE("Create instance")
     QAzureStorageRestApi api(username, pass);
 }
 
+TEST_CASE("Update credentials")
+{
+    QString username("fakeUser");
+    QString pass("fakePass");
+
+    QAzureStorageRestApi api(username, pass);
+
+    username = "fakeUser2";
+    pass = "fakePass2";
+    api.updateCredentials(username, pass);
+}
+
+TEST_CASE("List files (invalid response)")
+{
+    QString username("fakeUser");
+    QString pass("fakePass");
+    QString container = "invalidContainer";
+
+    QAzureStorageRestApi api(username, pass);
+
+    QNetworkReply* reply = api.listFiles(container);
+    REQUIRE(reply != nullptr);
+    REQUIRE(reply->error() == QNetworkReply::NetworkError::NoError);
+}
+
+TEST_CASE("Upload file (invalid path)")
+{
+    QString username("fakeUser");
+    QString pass("fakePass");
+    QString path = "invalidPath";
+    QString container = "invalidContainer";
+    QString blob = "invalidBlolName";
+
+    QAzureStorageRestApi api(username, pass);
+
+    QNetworkReply* reply = api.uploadFile(path, container, blob);
+    REQUIRE(reply == nullptr);
+}
+
+TEST_CASE("Upload file (invalid response)")
+{
+    QString username("fakeUser");
+    QString pass("fakePass");
+
+    QString path = "dummyFile.txt";
+    QFile file(path);
+    REQUIRE(file.open(QIODevice::WriteOnly));
+    {
+        QTextStream stream(&file);
+        stream << "Dummy information" << endl;
+    }
+    file.close();
+
+    QString container = "invalidContainer";
+    QString blob = "invalidBlolName";
+
+    QAzureStorageRestApi api(username, pass);
+
+    QNetworkReply* reply = api.uploadFile(path, container, blob);
+    REQUIRE(reply != nullptr);
+    REQUIRE(reply->error() == QNetworkReply::NetworkError::NoError);
+}
+
+TEST_CASE("Download file (invalid response)")
+{
+    QString username("fakeUser");
+    QString pass("fakePass");
+    QString container = "invalidContainer";
+    QString blob = "invalidBlolName";
+
+    QAzureStorageRestApi api(username, pass);
+
+    QNetworkReply* reply = api.downloadFile(container, blob);
+    REQUIRE(reply != nullptr);
+    REQUIRE(reply->error() == QNetworkReply::NetworkError::NoError);
+}
+
 int main(int argc, char* argv[])
 {
     QCoreApplication a(argc, argv);
